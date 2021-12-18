f4d689f044671ef52ac6cc0555d2fc74dc4df9f7
Rene Hexel <rhx@users.noreply.github.com>
2021-12-19 06:49:05 +1000

    Update for Swift 5.5.2

--- checkoutRelease.sh
+++ checkoutRelease.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 . "$(dirname $0)/utils.sh"
 
-SCHEME=release/5.5
+SCHEME=release/5.5.2
 
 echo "♻️  Resetting the repositories..."
 find . -maxdepth 1 -type d \( ! -name . \) -exec bash -c "[ -d '{}'/.git ] && echo ■ Cleaning '{}' && cd '{}' && git reset --hard HEAD && git clean -fd" \;
